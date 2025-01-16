33e161040ac5467d50c740610a7421f3481e2b36
parth <parthahuja74@gmail.com>
2025-01-16 23:58:26 +0530

    Max Histogram

--- src/Heap/MaxHeap.kt
+++ src/Heap/MaxHeap.kt
@@ -3,11 +3,11 @@ package Heap
 import java.util.PriorityQueue
 
 
-// Min heap will be used to find k largest element
+// Max heap will be used to find k smallest element
 fun main() {
     val input = mutableListOf<Int>(7, 10, 4, 3, 20, 15)
     val k = 3
-    val a = PriorityQueue<Int>()
+    val a = PriorityQueue<Int> { a, b -> b - a }
     input.map {
         a.add(it)
         println("peek is : " + a.peek())
